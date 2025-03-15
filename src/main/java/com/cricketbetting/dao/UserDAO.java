0a1f3d65c1bc1f98b9b06a4d83134d05438de9d5
Rapuru Lokesh <rapuru.lokesh@rategain.com>
2025-03-15 22:18:47 +0530

    Updated Dockerfile for Render deployment

--- src/main/java/com/cricketbetting/dao/UserDAO.java
+++ src/main/java/com/cricketbetting/dao/UserDAO.java
@@ -2,19 +2,21 @@ package com.cricketbetting.dao;
 
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDAO 
 {
+	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
-    public UserDAO(JdbcTemplate jdbcTemplate) 
-    {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-    
+	/*
+	 * public UserDAO(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate;
+	 * }
+	 */
     public List<Map<String,Object>> findAll() 
     {
     	List<Map<String,Object>> list =null;
